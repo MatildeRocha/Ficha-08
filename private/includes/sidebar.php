849051dc5874accfba575f98e49ce2ada9e96680
Matilde Rocha <1240962@isep.ipp.pt>
2026-05-20 21:08:48 +0100

    Alteração dos restantes ficheiros html para php

--- private/includes/sidebar.php
+++ private/includes/sidebar.php
@@ -1,19 +1,19 @@
 <aside class="col-md-3 col-lg-2 bg-secondary text-white p-3 min-vh-100">
     <h4>Menu</h4>
     <nav>
-        <a href="views/clientes/lista.html" class="nav-link text-white px-0 mb-2 d-block">
+        <a href="/Ficha%2008/private/views/clientes/lista.php" class="nav-link text-white px-0 mb-2 d-block">
             <i class="fas fa-users"></i> &ensp; Clientes
         </a>
-        <a href="views/agendamento/agendamento.html" class="nav-link text-white px-0 mb-2 d-block">
+        <a href="/Ficha%2008/private/views/agendamento/agendamento.php" class="nav-link text-white px-0 mb-2 d-block">
             <i class="fas fa-calendar-alt"></i> &ensp;Agendamentos
         </a>
-        <a href="views/planos/planos.html" class="nav-link text-white px-0 mb-2 d-block">
+        <a href="/Ficha%2008/private/views/planos/planos.php" class="nav-link text-white px-0 mb-2 d-block">
             <i class="fas fa-dumbbell"></i> &ensp;Planos de Treino
         </a>
-        <a href="views/equipamentos/equipamentos.html" class="nav-link text-white px-0 mb-2 d-block">
+        <a href="/Ficha%2008/private/views/equipamentos/equipamentos.php" class="nav-link text-white px-0 mb-2 d-block">
             <i class="fas fa-cogs"></i> &ensp;Equipamentos
         </a>
-        <a href="views/produtos-servicos/produtos-servicos.html"
+        <a href="/Ficha%2008/private/views/produtos-servicos/produtos-servicos.php"
             class="nav-link text-white px-0 mb-2 d-block">
             <i class="fas fa-box-open"></i>&ensp;Produtos e Serviços
         </a>
